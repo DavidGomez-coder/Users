a05aac7c33b5c7968b7cac744f05c5d7c3a85876
David Gómez <dgpv2000@gmail.com>
2022-10-10 14:20:11 +0200

    getAllUsers problem fixed

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,7 +6,24 @@ const addUser = async (req, res, next) => {
     try {
         const data = req.body;
         const user = await firestore.collection('users').doc().set(data);
-        res.send(`User with id ${data.id} and name ${data.name} has been added`);
+        res.send(`User with  name ${data.name} has been added`);
+
+    }catch(error){
+        res.status(400).send(error.message);
+    }
+}
+
+const getUser = async (req, res, next) => {
+    try {
+        const id = req.params.id;
+        const user = await firestore.collection('users').doc(id);
+        const data = await user.get();
+
+        if (!data.exists) {
+            res.status(404).send('There are not any user to display');
+        } else{
+            res.send(data.data());
+        }
 
     }catch(error){
         res.status(400).send(error.message);
@@ -23,7 +40,7 @@ const getAllUsers = async (req, res, next) => {
             res.status(404).send('There are not users to display');
         } else {
             data.forEach(atrb => {
-                const nUser = new User(atrb.data().id, atrb.data().name);
+                const nUser = new User(atrb.id, atrb.data().name);
                 users_collection.push(nUser);
             });
             res.send(users_collection);
@@ -35,7 +52,9 @@ const getAllUsers = async (req, res, next) => {
     }
 }
 
+
 module.exports = {
     addUser,
-    getAllUsers
+    getAllUsers,
+    getUser
 }
--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,11 +1,12 @@
 const express = require('express')
-const { addUser, getAllUsers } = require('../controllers/userController')
+const { addUser, getAllUsers, getUser } = require('../controllers/userController')
 
 const router = express.Router()
 
 //users routes
 router.post('/user/add', addUser);
-router.get('/user/all', getAllUsers);
+router.get('/user/:id', getUser);
+router.get('/users', getAllUsers);
 
 
 module.exports = {
